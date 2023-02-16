92d586116e81d21cc316eccc533153c2f46e8afa
Ezequiel Kumiec <103332721+EzequielKumiec@users.noreply.github.com>
2023-02-16 17:24:05 -0300

    Update Actividad1-4.sql

--- Actividad1-4.sql
+++ Actividad1-4.sql
@@ -18,9 +18,9 @@
 --having avg(cantidad) <= 56
 --and fecha = min(fecha)
 --------------------------------------------------------------------------------
---4. Se necesita un listado que informe sobre el monto máximo, mínimo y
---total que gastó en esta librería cada cliente el año pasado, pero solo
---donde el importe total gastado por esos clientes esté entre 300 y 800.
+--4. Se necesita un listado que informe sobre el monto mÃ¡ximo, mÃ­nimo y
+--total que gastÃ³ en esta librerÃ­a cada cliente el aÃ±o pasado, pero solo
+--donde el importe total gastado por esos clientes estÃ© entre 300 y 800.
 --------------------------------------------------------------------------------
 
 SELECT max(pre_unitario) 'Monto maximo',
@@ -35,7 +35,7 @@ group by nom_cliente, fecha
 having sum(pre_unitario * cantidad) between 300 and 800
 --------------------------------------------------------------------------------
 --5. Muestre la cantidad facturas diarias por vendedor; para los casos en que
---esa cantidad sea 2 o más.
+--esa cantidad sea 2 o mÃ¡s.
 --------------------------------------------------------------------------------
 
 select count(f.nro_factura) 'Cantidad de facturas',
@@ -47,9 +47,9 @@ from facturas f
 group by nom_vendedor, fecha
 having count(f.nro_factura) >= 2
 --------------------------------------------------------------------------------
---6. Desde la administración se solicita un reporte que muestre el precio
---promedio, el importe total y el promedio del importe vendido por artículo
---que no comiencen con “c”, que su cantidad total vendida sea 100 o más
+--6. Desde la administraciÃ³n se solicita un reporte que muestre el precio
+--promedio, el importe total y el promedio del importe vendido por artÃ­culo
+--que no comiencen con â€œcâ€, que su cantidad total vendida sea 100 o mÃ¡s
 --o que ese importe total vendido sea superior a 700.
 
 -------------------------------Detalle--------------------------------------------
@@ -78,9 +78,9 @@ where a.descripcion not like 'c%'
 group by a.descripcion
 having sum(cantidad) >= 100 or sum(cantidad * df.pre_unitario) > 700
 --------------------------------------------------------------------------------
---7. Muestre en un listado la cantidad total de artículos vendidos, el importe
---total y la fecha de la primer y última venta por cada cliente, para lo
---números de factura que no sean los siguientes: 2, 12, 20, 17, 30 y que el
+--7. Muestre en un listado la cantidad total de artÃ­culos vendidos, el importe
+--total y la fecha de la primer y Ãºltima venta por cada cliente, para lo
+--nÃºmeros de factura que no sean los siguientes: 2, 12, 20, 17, 30 y que el
 --promedio de la cantidad vendida oscile entre 2 y 6.
 --------------------------------------------------------------------------------
 
@@ -97,10 +97,10 @@ where df.nro_factura not in (2,12,20,17,30)
 group by df.nro_factura, fecha, nom_cliente
 having avg(cantidad * df.pre_unitario) between 2 and 6
 --------------------------------------------------------------------------------
---8. Emitir un listado que muestre la cantidad total de artículos vendidos, el
+--8. Emitir un listado que muestre la cantidad total de artÃ­culos vendidos, el
 --importe total vendido y el promedio del importe vendido por vendedor y 
---por cliente; para los casos en que el importe total vendido esté entre 200
---y 600 y para códigos de cliente que oscilen entre 1 y 5.
+--por cliente; para los casos en que el importe total vendido estÃ© entre 200
+--y 600 y para cÃ³digos de cliente que oscilen entre 1 y 5.
 --------------------------------------------------------------------------------
 
 select df.nro_factura 'Numero de Factura',
@@ -118,7 +118,7 @@ where c.cod_cliente between 1 and 5
 group by df.nro_factura,c.nom_cliente + ', ' + c.ape_cliente,c.cod_cliente,v.nom_vendedor + ', ' + v.ape_vendedor
 having sum(cantidad * pre_unitario) between 200 and 600
 --------------------------------------------------------------------------------
---9. ¿Cuáles son los vendedores cuyo promedio de facturación el mes
+--9. Â¿CuÃ¡les son los vendedores cuyo promedio de facturaciÃ³n el mes
 --pasado supera los $ 800?
 --------------------------------------------------------------------------------
 
@@ -133,7 +133,7 @@ group by v.nom_vendedor + ', ' + v.ape_vendedor, fecha
 having avg(cantidad * df.pre_unitario) > 800
 order by 3
 --------------------------------------------------------------------------------
---10.¿Cuánto le vendió cada vendedor a cada cliente el año pasado siempre
+--10.Â¿CuÃ¡nto le vendiÃ³ cada vendedor a cada cliente el aÃ±o pasado siempre
 --que la cantidad de facturas emitidas (por cada vendedor a cada cliente)
 --sea menor a 5?
 --------------------------------------------------------------------------------
@@ -152,4 +152,7 @@ group by c.nom_cliente + ', ' + c.ape_cliente ,
 	   v.nom_vendedor + ', ' + v.ape_vendedor,
 	   fecha
 having count(df.nro_factura) <5
---------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------
+--                                    Ezek                                             --
+-----------------------------------------------------------------------------------------
